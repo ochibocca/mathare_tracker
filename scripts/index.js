7bce7e14e56fc9edb45b0192b33feab724db9f2c
ochibocca <regan.oo-is.nairobi@stillirisengo.org>
2024-10-19 11:05:57 +0300

    added a folder for javascript

---
+++ scripts/index.js
@@ -0,0 +1,64 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let myVariables = true;
+let otherVariable = 10;
+// console.log(myVariable );
+let a = 2;
+let b = 3;
+ let myVariable = a+b;
+// myVariable = a+b;
+console.log(myVariable);
+console.log(myVariable = myVariables);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let t = "george";
+let k = "megan";
+console.log( t +" "+ k );
+console.log("george "+" megan");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(9-50);
+console.log(10/5);
+console.log(8*6);
+
+
+
+
+
+
+let r = 5;
+let q = 22;
+console.log( r === q );
+
+console.log(r !== q);
+
