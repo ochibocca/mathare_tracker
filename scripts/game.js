1f67abfd5ce27b81dfca70b6a502e0463edbae42
ochibocca <regan.oo-is.nairobi@stillirisengo.org>
2024-12-05 17:26:07 +0300

    adjusted the javascriupt

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,16 @@
 const startButton = document.getElementById('start-btn')
 startButton.addEventListener('click', startGame)
+const questionCointainerElement = document.getElementById('question-cointainer')
 
 
 
 function startGame(params) {
     console.log('started');
+    startButton.classList.add('hide')
+    questionCointainerElement.classList.remove('hide')
+    setNextQuestion(params)
+    
+
     
 }
 function setNextQuestion(params) {
