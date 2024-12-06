0565b501dd903281891315739e68666cdd37ced6
ochibocca <regan.oo-is.nairobi@stillirisengo.org>
2024-12-06 00:29:31 -0800

    worked on the js for game

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,16 @@
 const startButton = document.getElementById('start-btn')
 startButton.addEventListener('click', startGame)
 const questionCointainerElement = document.getElementById('question-cointainer')
+const shuffledQuestions, currentQuestionIndex 
 
 
 
 function startGame(params) {
     console.log('started');
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+
     questionCointainerElement.classList.remove('hide')
     setNextQuestion(params)
     
@@ -14,8 +18,21 @@ function startGame(params) {
     
 }
 function setNextQuestion(params) {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
     
 }
 function selectAnswer(params) {
     
 }
+const questions = [
+    {
+        question : 'What is Malaria',
+        answer: [
+            { text: 'It is a disease', correct: true},
+            { text: 'It is a game', correct: false}
+
+
+        ]
+
+    }
+]
