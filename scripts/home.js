81d8cd64a95a86bcb2312baad5ad56bb0cea66c2
ochibocca <regan.oo-is.nairobi@stillirisengo.org>
2025-02-22 12:56:37 +0300

    added the game level

---
+++ scripts/home.js
@@ -0,0 +1,9 @@
+  function myFunction() {
+    const hidden = document.getElementsByClassName('hide')
+    if (this.style.color === "black") {
+        this.style.color = "red"
+        
+    }else{
+        this.style.color = "black"
+    }
+}
