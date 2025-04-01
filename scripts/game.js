9f98f3ee8dcc681eb55abc127fb5472a6d78babc
ochibocca <regan.oo-is.nairobi@stillirisengo.org>
2025-04-01 15:58:46 +0300

    added javascript functions on login hampage

---
+++ scripts/game.js
@@ -0,0 +1,9 @@
+const question = document.getElementById("question-cointainer");
+const btn = document.getElementById("start-btn");
+
+function myFunction() {
+    if (question.display.style === "none") {
+        question.display.style = "block";
+    }
+    
+}
--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,19 @@
-const btn = document.getElementById('start-btn');
-function btn(params) {
-    StylePropertyMap.
+function myFunction() {
+    const container1 = document.getElementById('container1');
+    const loginForm = document.getElementById('loginForm');
+    if (container1.style.display === "none" ) {
+        container1.style.display = "block"
+        loginForm.style.display = "none"
+    }else{
+        loginForm.style.display = "block"
+        container1.style.display = "none"
+    }
+}
+function goBack() {
+    if (loginForm.style.display === "block") {
+        cointainer1.style.display = "none"
+    }else {
+        cointainer1.style.display = "block"
+        loginForm.style.display === "none"
+    }
 }
